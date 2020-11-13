edbec8ef33f11e234f8d85cbfc538f2b33a267d4
Kamila Sporek <kamilasporek@gmail.com>
2020-11-13 22:10:24 +0100

    updateWynik

--- js/script.js
+++ js/script.js
@@ -30,16 +30,4 @@ if(playerInput == '1'){
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-  printMessage('Ty wygrywasz!');
-} else if (computerMove == "papier" && playerMove == "nożyce") {
-  printMessage('Ty wygrywasz!');
-} else if (computerMove == "nożyce" && playerMove == "kamień")
-  printMessage('Ty wygrywasz!');
-} else if (playerMove == "nieznany ruch") {
-  printMessage('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
-} else if (computerMove == playerMove){
-  printMessage('Remis!')
-} else (){
-  printMessage('Niestety przegrywasz')
-}
+
