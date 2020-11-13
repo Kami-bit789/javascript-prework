ade0f559a86ef8cdfaf6c2105395eab007cf07fa
Kamila Sporek <kamilasporek@gmail.com>
2020-11-13 21:45:48 +0100

    updateWynik

--- js/script.js
+++ js/script.js
@@ -32,4 +32,10 @@ printMessage('Twój ruch to: ' + playerMove);
 
 if( computerMove == 'kamień' && playerMove == 'papier'){
   printMessage('Ty wygrywasz!');
+} else if (computerMove == "papier" && playerMove == "nożyce") {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == "nożyce" && playerMove == "kamień")
+  printMessage('Ty wygrywasz!');
+} else (){
+  printMessage('Niestety przegrywasz')
 }
