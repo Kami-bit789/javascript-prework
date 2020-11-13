63934d4140c2a2963c28bf01aeb3d07af337deb4
Kamila Sporek <kamilasporek@gmail.com>
2020-11-13 22:01:25 +0100

    updateBrackets

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if(randomNumber == '1'){
   computerMove = 'kamień';
-} else if (randomNumber == 2) {
+} else if (randomNumber == '2') {
     computerMove = "papier"
-} else if (randomNumber == 3) {
+} else if (randomNumber == '3') {
     computerMove = "nożyce"
 }
 
