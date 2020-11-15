a871bbef56f74fdf53d74011eb2b3cdbc6ac02c8
Kamila Sporek <kamilasporek@gmail.com>
2020-11-15 21:43:19 +0100

    displayResult

--- js/script.js
+++ js/script.js
@@ -32,19 +32,6 @@ if(playerInput == '1'){
 */
 
 let result = displayResult(argComputerMove, argPlayerMove);
-printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-  
-	if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-  printMessage('Ty wygrywasz!');
-  } else if (argComputerMove == "papier" && argPlayerMove == "nożyce") {
-  printMessage('Ty wygrywasz!');
-  } else if (argComputerMoveMove == "nożyce" && argPlayerMove == "kamień") {
-  printMessage('Ty wygrywasz!');
-  } else if (argPlayerMove == "nieznany ruch") {
-  printMessage('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
-  } else if (argComputerMove == argPlayerMove){
-  printMessage('Remis!')
-  } else {
-  printMessage('Niestety przegrywasz')
-  } 
+printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove, 'wynik:' + displayResult(argComputerMove, argPlayerMove);
+   
 
