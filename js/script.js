62351eaa931f50edece303f08687b3e0fc3f7195
Kamila Sporek <kamilasporek@gmail.com>
2020-11-15 21:24:36 +0100

    reult

--- js/script.js
+++ js/script.js
@@ -31,4 +31,4 @@ if(playerInput == '1'){
 */
 printMessage('Twój ruch to: ' + getMoveName);
 
-function displayResult (argComputerMove, argPlayerMove);
+let displayResult (argComputerMove, argPlayerMove);
