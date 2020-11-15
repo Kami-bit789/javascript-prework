ec79630dde316cd28a209461fb63667d32296bc0
Kamila Sporek <kamilasporek@gmail.com>
2020-11-15 20:44:11 +0100

    GetMove

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = getMoveName(randomNumber); 
-if (getMoveName == '1'){
+if (randomNumber == '1'){
   computerMove = 'kamień';
-} else if (getMoveName == '2'){
+} else if (randomNumber == '2'){
   computerMove = "papier"
-} else if (getMoveName == '3'){
+} else if (randomNumber == '3'){
   computerMove = "nożyce"
 }
 /*
