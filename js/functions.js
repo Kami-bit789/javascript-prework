95c1262cf8bf08c30d6d8a5b8c7ec79edebd0990
Kamila Sporek <kamilasporek@gmail.com>
2020-11-15 20:49:59 +0100

    getmove

--- js/functions.js
+++ js/functions.js
@@ -7,18 +7,12 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-function displayResult (argComputerMove, argPlayerMove){
-		if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-		printMessage('Ty wygrywasz!');
-	  } else if (argComputerMove == "papier" && argPlayerMove == "nożyce") {
-		printMessage('Ty wygrywasz!');
-	  } else if (argComputerMoveMove == "nożyce" && argPlayerMove == "kamień") {
-		printMessage('Ty wygrywasz!');
-	  } else if (argPlayerMove == "nieznany ruch") {
-		printMessage('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
-	  } else if (argComputerMove == argPlayerMove){
-		printMessage('Remis!')
-	  } else {
-		printMessage('Niestety przegrywasz')
-	  }	
+function getMoveName(randomNumber) {
+		if (randomNumber == '1'){
+		getMoveName = 'kamień';
+	  } else if (randomNumber == '2'){
+		getMoveName = "papier"
+	  } else if (randomNumber == '3'){
+		getMoveName = "nożyce"
+	  }
 }
