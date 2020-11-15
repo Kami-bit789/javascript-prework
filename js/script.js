3d3296603b20dd0e5d413ecb9d179999d2b424ae
Kamila Sporek <kamilasporek@gmail.com>
2020-11-15 20:22:46 +0100

    updateGetMoveName

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = getMoveName(randomNumber);
+if (randomNumber == '1'){
+  getMoveName = 'kamień';
+} else if (randomNumber == '2'){
+  getMoveName = "papier"
+} else if (randomNumber == '3'){
+  getMoveName = "nożyce"
+}
 /*
 if(randomNumber == '1'){
   computerMove = 'kamień';
@@ -18,8 +25,8 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
+let playerMove = getMoveName(randomNumber);
+/*
 if(playerInput == '1'){
   playerMove = 'kamień';
 } else if (playerInput == '2') {
@@ -27,7 +34,7 @@ if(playerInput == '1'){
 } else if (playerInput == "3") {
   playerMove = "nożyce"
 }
-
+*/
 printMessage('Twój ruch to: ' + playerMove);
 
 
