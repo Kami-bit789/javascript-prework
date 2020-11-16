b0d431e4bcd86ac5201e54cc129916d79cd59613
Kamila Sporek <kamilasporek@gmail.com>
2020-11-16 21:04:13 +0100

    functionupdate

--- js/script.js
+++ js/script.js
@@ -21,6 +21,11 @@ function playGame(playerInput){
   return "nożyce";
   }
 }
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+
 
 function displayResult(computerMove, playerMove){
   printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
@@ -38,33 +43,42 @@ function displayResult(computerMove, playerMove){
   } 
 }
 displayResult(computerMove, playerMove); 
-
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-  console.log('Wylosowana liczba to: ' + randomNumber);
-
-  let computerMove = getMoveName(randomNumber);
-
- 
-
-  printMessage('Mój ruch to: ' + getMoveName(randomNumber));
   /*
   let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
   
   console.log('Gracz wpisał: ' + playerInput);
   */
-  
-  
- 
+ let computerMove = getMoveName(randomNumber);
+ printMessage('Mój ruch to: ' + getMoveName(randomNumber));
+
 }
-document.getElementById('play-rock').addEventListener('click', function(){
-  printMessage('Guzik "Kamień" został kliknięty');
+
+document.getElementById('play-rock').addEventListener('click', playGame(playerInput){
+    if(playerInput == '1') {
+    playerMove =  'kamień';
+    } else if (playerInput == '2') {
+    playerMove =  "papier";
+    } else if (playerInput == "3") {
+    playerMove =  "nożyce";
+    }
 });
 
-document.getElementById('play-paper').addEventListener('click', function(){
-  printMessage('Guzik "Papier" został kliknięty');
+document.getElementById('play-paper').addEventListener('click', playGame(playerInput){
+  if(playerInput == '1') {
+  playerMove =  'kamień';
+  } else if (playerInput == '2') {
+  playerMove =  "papier";
+  } else if (playerInput == "3") {
+  playerMove =  "nożyce";
+  }
 });
 
-document.getElementById('play-scrissors').addEventListener('click', function(){
-  printMessage('Guzik "Nożyce" został kliknięty');
+document.getElementById('play-scrissors').addEventListener('click', playGame(playerInput){
+  if(playerInput == '1') {
+  playerMove =  'kamień';
+  } else if (playerInput == '2') {
+  playerMove =  "papier";
+  } else if (playerInput == "3") {
+  playerMove =  "nożyce";
+  }
 });
