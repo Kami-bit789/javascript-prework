1cf1c2ad1917e57332882e2cd5117c37fb2bcec0
Kamila Sporek <kamilasporek@gmail.com>
2020-11-16 11:31:17 +0100

    computerMove

--- js/script.js
+++ js/script.js
@@ -2,36 +2,24 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = getMoveName(MoveId);
+let computerMove = 'nieznany ruch';
 
-function getMoveName(MoveId) {
-	if (MoveId == '1'){
-	return 'kamień';
-	} else if (MoveId == '2'){
-  return "papier"
-	} else if (MoveId == '3'){
-  return "nożyce"
+	if (randomNumber == '1'){
+	computerMove = 'kamień';
+	} else if (randomNumber == '2'){
+  computerMove = "papier"
+	} else if (randomNumber == '3'){
+  computerMove = "nożyce"
 	}
-} 
-printMessage('Mój ruch to: ' + getMoveName);
 
-/*
-if(randomNumber == '1'){
-  computerMove = 'kamień';
-} else if (randomNumber == '2') {
-    computerMove = "papier"
-} else if (randomNumber == '3') {
-    computerMove = "nożyce"
-}
-*/
+printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(playerInput);
-printMessage('Twój ruch to: ' + getMoveName);
-/*
+let playerMove = 'nieznany ruch';
+
 if(playerInput == '1'){
   playerMove = 'kamień';
 } else if (playerInput == '2') {
@@ -39,23 +27,4 @@ if(playerInput == '1'){
 } else if (playerInput == "3") {
   playerMove = "nożyce"
 }
-*/
-
-function displayResult(argComputerMove, argPlayerMove){
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-  if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-  printMessage('Ty wygrywasz!');
-  } else if (argComputerMove == "papier" && argPlayerMove == "nożyce") {
-  printMessage('Ty wygrywasz!');
-  } else if (argComputerMove == "nożyce" && argPlayerMove == "kamień") {
-  printMessage('Ty wygrywasz!');
-  } else if (argPlayerMove == "nieznany ruch") {
-  printMessage('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
-  } else if (argComputerMove == argPlayerMove){
-  printMessage('Remis!')
-  } else {
-  printMessage('Niestety przegrywasz')
-  } 
-}
-
-displayResult(computerMove, playerMove);
+printMessage('Twój ruch to: ' + playerMove);
